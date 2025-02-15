7a7aea157f7045265bae94258944268fcf1052dd
ronny-souza <ronnymarinho2@gmail.com>
2025-02-15 12:09:21 -0300

    🎉 init: configurações iniciais da aplicação

---
+++ src/test/java/br/com/marinholab/keycloakxp/KeycloakxpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marinholab.keycloakxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakxpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
