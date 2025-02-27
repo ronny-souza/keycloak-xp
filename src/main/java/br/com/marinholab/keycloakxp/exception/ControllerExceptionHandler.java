fb6c2c7b7ad3d0bf06bb9ed2d81675e4ed386717
ronny-souza <ronnymarinho2@gmail.com>
2025-02-27 07:55:09 -0300

    ♻️ refactor: melhoria no tratamento de exceções

--- src/main/java/br/com/marinholab/keycloakxp/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/marinholab/keycloakxp/exception/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package br.com.marinholab.keycloakxp.exception;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    public ControllerExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @ExceptionHandler(CreateUserException.class)
     public ResponseEntity<ProblemDetail> handleCreateUserException(CreateUserException exception) {
@@ -20,5 +28,17 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ProblemDetail> handleMethodArgumentNotFoundException(MethodArgumentNotValidException exception) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+    }
+
+    @ExceptionHandler(KeycloakXpException.class)
+    public ResponseEntity<ProblemDetail> handleKeycloakXpException(KeycloakXpException exception) {
+        String translatedMessage = this.messageSource.getMessage(
+                exception.getTranslationCode(),
+                exception.getArgsAsArray(),
+                LocaleContextHolder.getLocale()
+        );
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(exception.getHttpStatus(), translatedMessage);
+        return ResponseEntity.status(exception.getHttpStatus()).body(problemDetail);
     }
 }
