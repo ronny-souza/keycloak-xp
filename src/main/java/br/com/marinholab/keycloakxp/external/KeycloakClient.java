b970e847bd44daa6a798fbea6639432c3dbee30c
ronny-souza <ronnymarinho2@gmail.com>
2025-03-02 10:31:31 -0300

    📚 docs: adicionando JavaDocs no client do Keycloak, para deixar mais clara a funcionalidade e seus parâmetros

--- src/main/java/br/com/marinholab/keycloakxp/external/KeycloakClient.java
+++ src/main/java/br/com/marinholab/keycloakxp/external/KeycloakClient.java
@@ -1,6 +1,7 @@
 package br.com.marinholab.keycloakxp.external;
 
-import br.com.marinholab.keycloakxp.core.model.LoginResponseDTO;
+import br.com.marinholab.keycloakxp.core.model.AccessTokenDTO;
+import br.com.marinholab.keycloakxp.core.model.properties.KeycloakProperties;
 import jakarta.ws.rs.core.MediaType;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +14,36 @@ import java.util.Map;
 @FeignClient(name = "keycloak-client", url = "${keycloak.server}", path = "/realms")
 public interface KeycloakClient {
 
+    /**
+     * This endpoint, provided by the Keycloak API, is capable of performing a number of operations.
+     * The main ones include user login and token refresh.
+     *
+     * @param realm The realm of Keycloak environment configured on application properties, which can
+     *              be found in {@link KeycloakProperties }
+     * @param body  A map representing the form to be sent to Keycloak. The information contained generally
+     *              depens on the chosen grant_type.
+     * @return An {@link AccessTokenDTO} object containing the access token, refresh token, and information
+     * such as the expiration time for both.
+     */
     @PostMapping(
             value = "/{realm}/protocol/openid-connect/token",
             consumes = MediaType.APPLICATION_FORM_URLENCODED,
             produces = MediaType.APPLICATION_JSON
     )
-    LoginResponseDTO authenticate(@PathVariable final String realm, Map<String, ?> body);
+    AccessTokenDTO authenticateByGrantType(@PathVariable final String realm, Map<String, ?> body);
 
+    /**
+     * This endpoint, provided by the Keycloak API, is capable of destroying the user's session.
+     * What actually happens is that the refresh token becomes invalid, so the access token will remain
+     * available until it expires, and it will not be possible to renew it.
+     *
+     * @param realm         The realm of Keycloak environment configured on application properties, which can
+     *                      be found in {@link KeycloakProperties }
+     * @param authorization A string containing the authenticated user's current access token.
+     * @param body          A map representing the form to be sent to Keycloak. The information contained generally
+     *                      depens on the chosen grant_type.
+     * @return A String containing possible additional information, usually in case of errors.
+     */
     @PostMapping(
             value = "/{realm}/protocol/openid-connect/logout",
             consumes = MediaType.APPLICATION_FORM_URLENCODED,
