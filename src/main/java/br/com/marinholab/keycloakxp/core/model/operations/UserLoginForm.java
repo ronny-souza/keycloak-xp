d4deaa924e98dca13ba2f8b9d2dcabd53e0e27c3
ronny-souza <ronnymarinho2@gmail.com>
2025-03-02 10:26:36 -0300

    📚 docs: adicionando descrição as classes de modelo para documentação do swagger

--- src/main/java/br/com/marinholab/keycloakxp/core/model/operations/UserLoginForm.java
+++ src/main/java/br/com/marinholab/keycloakxp/core/model/operations/UserLoginForm.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+@Schema(description = "Representation of the payload required for the user authentication operation.")
 public record UserLoginForm(@Schema(description = "Unique username")
                             @NotBlank
                             String username,
