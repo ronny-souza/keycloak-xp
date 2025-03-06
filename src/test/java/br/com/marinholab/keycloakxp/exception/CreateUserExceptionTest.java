a3abf5b0de8dba64593a7c3b3a1f088c8d9b9fe4
ronny-souza <ronnymarinho2@gmail.com>
2025-03-05 23:05:26 -0300

    ✨ feat: complementando testes unitários da exceção de criação do usuário

--- src/test/java/br/com/marinholab/keycloakxp/exception/CreateUserExceptionTest.java
+++ src/test/java/br/com/marinholab/keycloakxp/exception/CreateUserExceptionTest.java
@@ -25,4 +25,13 @@ class CreateUserExceptionTest {
         assertEquals(HttpStatus.CONFLICT, exception.getHttpStatus());
         assertEquals(CreateUserException.USER_CREATION_CONFLICT_MESSAGE, exception.getTranslationCode());
     }
+
+    @Test
+    @DisplayName("Should return a valid instance of exception with username and status constructor when is not conflict")
+    void shouldReturnValidInstanceOfExceptionWithUsernameAndStatusConstructorWhenIsNotConflict() {
+        CreateUserException exception = new CreateUserException(HttpStatus.BAD_REQUEST, "root");
+        assertEquals(1, exception.getArgsAsArray().length);
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+        assertEquals(CreateUserException.DEFAULT_USER_CREATION_FAILURE_MESSAGE, exception.getTranslationCode());
+    }
 }
