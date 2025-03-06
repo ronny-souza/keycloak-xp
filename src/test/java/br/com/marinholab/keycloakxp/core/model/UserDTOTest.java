9bfed2fcbddba00d06a4f1cc77330771a95f024e
ronny-souza <ronnymarinho2@gmail.com>
2025-03-05 23:17:53 -0300

    ✨ feat: complementando demais testes unitários da aplicação

--- src/test/java/br/com/marinholab/keycloakxp/core/model/UserDTOTest.java
+++ src/test/java/br/com/marinholab/keycloakxp/core/model/UserDTOTest.java
@@ -69,4 +69,33 @@ class UserDTOTest {
         assertEquals(lastName, userDTO.lastName());
         assertEquals(role, userDTO.role());
     }
+
+    @Test
+    @DisplayName("Should return a valid instance of UserDTO with user representation constructor when roles is not informed")
+    void shouldReturnValidInstanceOfUserDTOWithUserRepresentationConstructorWhenRoleIsNotInformed() {
+        String id = "id";
+        String username = "username";
+        String email = "email";
+        String firstName = "firstName";
+        String lastName = "lastName";
+        String role = "USER";
+
+        UserRepresentation userRepresentationAsMock = mock(UserRepresentation.class);
+        when(userRepresentationAsMock.getId()).thenReturn(id);
+        when(userRepresentationAsMock.getUsername()).thenReturn(username);
+        when(userRepresentationAsMock.getEmail()).thenReturn(email);
+        when(userRepresentationAsMock.getFirstName()).thenReturn(firstName);
+        when(userRepresentationAsMock.getLastName()).thenReturn(lastName);
+        when(userRepresentationAsMock.getRealmRoles()).thenReturn(null);
+
+        UserDTO userDTO = new UserDTO(userRepresentationAsMock);
+
+        assertNotNull(userDTO);
+        assertEquals(id, userDTO.id());
+        assertEquals(username, userDTO.username());
+        assertEquals(email, userDTO.email());
+        assertEquals(firstName, userDTO.firstName());
+        assertEquals(lastName, userDTO.lastName());
+        assertEquals(role, userDTO.role());
+    }
 }
