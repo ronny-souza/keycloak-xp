07e1b1b9542ed126a6780a5c8f7d53d660089808
ronny-souza <ronnymarinho2@gmail.com>
2025-03-09 15:09:33 -0300

    ✨ feat: endpoint para desativação da conta do usuário da sessão

---
+++ src/main/java/br/com/marinholab/keycloakxp/core/model/common/JwtProperties.java
@@ -0,0 +1,10 @@
+package br.com.marinholab.keycloakxp.core.model.common;
+
+public class JwtProperties {
+
+    public static final String PREFERRED_USERNAME = "preferred_username";
+
+    private JwtProperties() {
+
+    }
+}
